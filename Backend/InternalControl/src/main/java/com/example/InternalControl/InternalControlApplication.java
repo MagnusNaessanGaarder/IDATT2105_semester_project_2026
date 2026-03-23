73c753e3563d8b4b0dc48fb84a93d9dd07203bbc
rexvelde <harald.helland.velde@gmail.com>
2026-03-23 16:09:28 +0100

    Initialized backend in Springbooot

---
+++ Backend/InternalControl/src/main/java/com/example/InternalControl/InternalControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.InternalControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalControlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InternalControlApplication.class, args);
+  }
+
+}
