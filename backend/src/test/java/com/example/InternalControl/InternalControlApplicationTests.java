10583e2e70c508e1ace15bb4d3e09f02bcd3e483
aninstan <aninstan@outlook.com>
2026-04-01 12:20:02 +0200

    Add test profile and Blob configuration to InternalControlApplicationTests

--- backend/src/test/java/com/example/InternalControl/InternalControlApplicationTests.java
+++ backend/src/test/java/com/example/InternalControl/InternalControlApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.InternalControl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class InternalControlApplicationTests {
+@ActiveProfiles("test")
+@Import(TestBlobConfig.class)
+class InternalControlApplicationTests extends AbstractIntegrationTest  {
 
     @Test
     void contextLoads() {
